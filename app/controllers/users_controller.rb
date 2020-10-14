27f0a47a6d01833510ae9e73466ad90595641565
vikash.agarwal <agarwalvikash209@gmail.com>
2020-10-14 19:28:33 +0530

    Normal User activity DONE

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,36 @@ class UsersController < ApplicationController
 
 	def index
     redirect_to '/users/sign_in' unless user_signed_in?
+    redirect_to edit_user_path(current_user.id) unless current_user.is_admin
     respond_to do |format|
       format.html
       format.json { render json: UserDatatable.new(view_context, current_user, params) }
     end
 	end
 
+  def new
+    redirect_to edit_user_path(current_user.id) unless current_user.is_admin
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid? && @user.save!
+      redirect_to({ action: 'index' }, notice: 'User was successfully created.')
+    else
+      flash[:error] = @user.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
   def edit
+    params[:id] = current_user.id unless current_user.is_admin #restricting to edit other user details
   end
 
   def update
     if @user.update(user_params)
       bypass_sign_in(current_user) if @user == current_user
-      redirect_to(users_path, notice: 'User was successfully updated.')
+      redirect_to(current_user.is_admin ? users_path : edit_user_path(current_user.id), notice: 'User was successfully updated.')
     else
       flash[:error] = @user.errors.full_messages.join(', ')
       redirect_to(edit_user_path)
